0d50134867f1c8c60ae1cc960df5b3a82dc4f35b
joelittyab-git <joeittab@gmail.com>
2023-09-21 22:07:02 +0530

    edited AuthenticationMiddleware.java

--- src/main/java/com/example/springjpa/Middlewares/AuthenticationMiddleware.java
+++ src/main/java/com/example/springjpa/Middlewares/AuthenticationMiddleware.java
@@ -4,19 +4,24 @@ package com.example.springjpa.Middlewares;
 import com.example.springjpa.objects.MutableHttpServletRequest;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 
-
 @Component
+@Order(2)
 public class AuthenticationMiddleware implements Filter {
 
+    Logger log = LoggerFactory.getLogger(AuthenticationMiddleware.class);
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         MutableHttpServletRequest mutableRequest = new MutableHttpServletRequest((HttpServletRequest) request) ;
         mutableRequest.setHeader("user","admin");
+        log.info("middleware:passed authentication");
 
 
 
